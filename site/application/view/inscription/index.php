f00c54966133682137cbb8530efa8169d320d9d2
Devil62 <devil.css@hotmail.fr>
2015-04-17 10:17:25 +0200

    Première mise en place du formulaire inscription utilisateurs

--- site/application/view/inscription/index.php
+++ site/application/view/inscription/index.php
@@ -1,3 +1,124 @@
 <div>
-   page d'inscription
+  <form class="form-horizontal col-md-8 col-md-offset-2">
+  <div class="form-group">
+    <legend>Inscription</legend>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="Nom" class="col-md-5 control-label">Nom de famille : </label>
+      <div class="col-md-7">
+        <input type="text" class="form-control" id="Nom">
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="text" class="col-md-5 control-label">Prénom : </label>
+      <div class="col-md-7">
+        <input type="text" class="form-control" id="Prénom">
+      </div>
+    </div>
+  </div>
+  
+  
+  <div class="row">
+    <div class="form-group">
+    <label for="Pays" class="col-md-5 control-label">Pays : </label>
+      <div class="col-md-7">
+        <select id="Pays" class="form-control" >
+          <option>France</option>
+        </select>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="Ville" class="col-md-5 control-label">Ville : </label>
+      <div class="col-md-7">
+        <input type="text" class="form-control" id="Ville">
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="Email" class="col-md-5 control-label">E-mail : </label>
+      <div class="col-md-7">
+        <input type="email" class="form-control" id="Email">
+      </div>
+    </div>
+  </div>
+  
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="naisssance" class="col-md-2 control-label">Date de naisssance : </label>
+      <div class="col-md-10">
+        <input type="date" class="form-control" id="naisssance">
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="profact" class="col-md-2 control-label">Profession ou études actuelles : </label>
+      <div class="col-md-10">
+        <input type="text" class="form-control" id="profact">
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="profant" class="col-md-5 control-label">Profession ou études antérieures : </label>
+      <div class="col-md-7">
+        <input type="text" class="form-control" id="profant">
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+    <label for="Formation" class="col-md-2 control-label">Études à l'IUT : </label>
+      <div class="col-md-10">
+        <select id="Formation" class="form-control" >
+          <option>TC</option>
+          <option>GEA</option>
+          <option>MMI</option>
+          <option>INFO</option>
+        </select>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <label for="photo" class="col-md-2 control-label">Photo de profil : </label>
+      <div class="col-md-10">
+        <input type="file" class="file" id="Photo">
+      </div>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="form-group">
+      <div class="col-md-10">
+        <input type="checkbox" class="checkbox" id="accept"> j’accepte les règles de protection de mes données
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  
+  
+  <div class="form-group">
+    <button class="pull-right btn btn-default">Envoyer</button>
+  </div>
+</form>
 </div>
