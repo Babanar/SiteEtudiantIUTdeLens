387bc2fcce91b1b4af2b489a818018a8f0b29e6a
Devil62 <devil.css@hotmail.fr>
2015-05-11 11:54:18 +0200

    Mise en place de la vue conversation

--- site/application/view/message/conversation.php
+++ site/application/view/message/conversation.php
@@ -1,25 +1,47 @@
 <div id="index_message">
     <div class="row">
       <h3>
-        Louis Behague, Mathieu Blanquin
+        Vous & Louis Behague
       </h3>
       <hr>
     </div>
 
 
 	<div class="row">
-	<div class="row">
-	<div class="col-md-offset-3 col-md-6" id="conv_view">
-				CONVERSATION
-		</div>
+		<div class="row">
+			<div class="col-md-offset-3 col-md-6" id="conv_view">
+				
+					<div class="row">
+						<div class="col-md-offset-1">
+							<div id="photoconv" class="col-md-2">	
+							Photo
+							</div>
+							<div class="col-md-4" id="auteur">Louis Behague</div>
+							<div class="col-md-4 col-md-offset-2" id="datetime">11/05/2015 08:39</div>
+							<div class="col-md-offset-3" id="message" >Mathieu, t'es vraiment BG <3 <3 <3 <3 <3 <3 <3</div>
+						</div>	
+					</div>
+				
+					<hr>
+				
+					<div class="row">
+						<div class="col-md-offset-1">
+							<div id="photoconv" class="col-md-2">	
+							Photo
+							</div>
+							<div class="col-md-4" id="auteur">Vous</div>
+							<div class="col-md-4 col-md-offset-2" id="datetime">11/05/2015 08:45</div>
+							<div class="col-md-offset-3" id="message" >Ouais je sais, Keur</div>
+						</div>	
+					</div>
+				
+			</div>
 		</div>
 		
 		<div class="row">
 			<textarea class="col-md-offset-3 col-md-6" cols="50"></textarea>
-			<button type="submit" class="col-md-1 btn btn-default" id="send_btn">ENVOYER</button>
-		
+				<button type="submit" class="col-md-1 btn btn-default" id="send_btn">ENVOYER</button>
 		</div>
-	
 	</div>
 </div>
 	
