f59aabcb6d3ffe43c06b12ccab6b0a8cf506a586
Babanar <louis.behague@orange.fr>
2015-06-01 09:40:42 +0200

    Vue du forum mis à jour

--- site/application/view/forum/index.php
+++ site/application/view/forum/index.php
@@ -5,36 +5,42 @@
       <h3>
         <span class="forumInfoPage">Forum Info</span>
       </h3>
-      <hr>
+      <hr class="departement_color">
     </div>
 
     <div class="row">
-        <div class="col-md-10 col-md-offset-1 forumcat_view" >
+        <div class="col-md-10 col-md-offset-1 forumcat_view departement_color_border" >
         
             <div class="row">
             <h3>
                 <span class="stagesCatTitre">Général</span>
             </h3>
-            <hr>
+            <hr class="departement_color">
             </div>
             
             <div class="row">
-                    <div class="col-md-10 col-md-offset-1 messageForum">
+                    <div class="col-md-10 col-md-offset-1 messageForum departement_color">
                     
                         <div class="col-md-2">
-                            <img src="../public/img/icone_msg.png" class="ico_no_new_msg">
-                            <div class="row"><p>103 messages au total</p></div>
+                            <div class="row icone_forum_enveloppe">
+                                 <img src="../public/img/icone_msg.png" class="ico_no_new_msg">
+                            </div>
+                            <div class="row">
+                                 103 messages
+                            </div>                        
                         </div>
                         
                         <div class="col-md-10">
-                            <div class="row titretopic"><h3>Bienvenue !</h3></div>
-                            <div class="row"><p class="lastMSG">Bonjour à tous, bienvenue sur le forum. BlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablabla</p></div>
-                            <div  class="row"><p class="InfoMSG">Par Mathieu Blanquin le vendredi 22 mai à 13:48</p></div>
+                            <div class="row titretopic "><h4><b>Bienvenue !</b></h4></div>
+                            <hr style="margin-top: 5px;">
+                            <div class="row"><p class="lastMSG">Bonjour à tous, bienvenue sur le forum. BlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablablaBlablablabla
+                                     BlablablablaBlablablablaBlablablablaBlablablabl BlablablablaBlablablablaBlablablablaBlablablabl BlablablablaBlablablablaBlablablablaBlablablabl BlablablablaBlablablablaBlablablablaBlablablabl</p></div>
+                            <div  class="row text-left"><p class="InfoMSG">Par Mathieu Blanquin le vendredi 22 mai à 13:48</p></div>
                         </div>
                     </div>
             </div>
             
-            <hr>
+            <hr class="departement_color">
             
             <div class="row">
                     <div class="col-md-10 col-md-offset-1 messageForum">
@@ -60,7 +66,7 @@
 </div>
     
     <div class="row">
-        <div class="col-md-10 col-md-offset-1 forumcat_view">
+        <div class="col-md-10 col-md-offset-1 forumcat_view ">
         
             <div class="row">
             <h3>
@@ -115,4 +121,6 @@
 
 
 
-
+<script>
+   initialiseDepartementColor();
+</script>
