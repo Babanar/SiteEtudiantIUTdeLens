1030e6a15073ada0ec96a56117b5b5a54ddb220b
Devil62 <devil.css@hotmail.fr>
2015-06-04 11:27:56 +0200

    Finalisation des CGU

--- site/application/view/_templates/mentions_legales.php
+++ site/application/view/_templates/mentions_legales.php
@@ -4,6 +4,7 @@
             <h3>
                 <span id="titrementions_légalesPage">Mentions légales </span>
             </h3>
+            Ecrit le 3 juin 2015. Dernière modification le 4 juin 2015 
             <hr>
         </div>
 
@@ -136,41 +137,263 @@
                     <div class="col-md-offset-1" id="informations_personnelles_stockees">
 
 
-                    <div class="row">
-                        <h4>
-                            <span id="informations_personnelles_titre">Informations personelles stockées</span>
-                        </h4>
-                        <hr>
-                    </div>
+                        <div class="row">
+                            <h4>
+                                <span id="informations_personnelles_titre">Informations personelles stockées</span>
+                            </h4>
+                            <hr>
+                        </div>
 
-                    <div class="row">
-                        Les données stockées dans le site différent s'il sagit d'une personne physique ou d'une entreprises.  
-                        Dans chacun des cas, ces données peuvent être regroupées en deux catégories celles qui doivent obligatoirement être renseignées
-                        dans le but d'identifier un minimum chaque individu, et celles qui sont facultatives, 
-                        qui permettent uniquement d'identifier l'individu de façon plus précise.
-                    </div>
+                        <div class="row">
+                            Les données stockées dans le site différent s'il sagit d'une personne physique ou d'une entreprises.  
+                            Dans chacun des cas, ces données peuvent être regroupées en deux catégories celles qui doivent obligatoirement être renseignées
+                            dans le but d'identifier un minimum chaque individu, et celles qui sont facultatives, 
+                            qui permettent uniquement d'identifier l'individu de façon plus précise.
+                        </div>
 
 
-                    <div class="row">
-                        <h5>
-                            <span id="informations_personnelles_entreprises_titre">Concernant les entreprises, les informations obligatoires sont :</span>
-                        </h5>
-                    </div> 
-                    
-                    <div id="row">
-                        <ul>
-                            <li>Le nom de l'entreprise</li>
-                            <li>Le secteur d'activité : afin d'identifie run minimum les activités de l'entreprise.</li>
-                            <li>Le numéro de Siret : Ce nombre composé de 14 chiffres est un identifiant national UNIQUE attribuer à chaque entreprise.</li>
-                        </ul>
-                        
-                        
+                        <div class="row">
+                            <h5>
+                                <span id="informations_personnelles_obligatoire_entreprises_titre">Concernant les entreprises, les informations obligatoires sont :</span>
+                            </h5>
+                        </div> 
+
+                        <div class="row">
+                            <ul>
+                                <li>Le nom de l'entreprise</li>
+                                <li>Le secteur d'activité : afin d'identifie run minimum les activités de l'entreprise.</li>
+                                <li>Le numéro de Siret : Ce nombre composé de 14 chiffres est un identifiant national UNIQUE attribué à chaque entreprise.</li>
+                            </ul>
+                        </div>
+
+                        <div class="row">
+                            <h5>
+                                <span id="informations_personnelles_entreprises_facultatives_titre">les informations facultatives sont :</span>
+                            </h5>
+                        </div> 
+
+                        <div class="row">
+                            <ul>
+                                <li>l'adresse postale de l'entreprise</li>
+                                <li>le numéro de téléphone</li>
+                                <li>La photo de profil</li>
+                            </ul>
+                        </div>
+
+                        <div class="row">
+                            <h5>
+                                <span id="informations_personnelles_obligatoire_personnes_titre">Concernant les personnes, les informations obligatoires sont :</span>
+                            </h5>
+                        </div> 
+
+                        <div class="row">
+                            <ul>
+                                <li>Le nom de famille</li>
+                                <li>Le prénom</li>
+                                <li>La date de naissance</li>
+                                <li>Les formations suivies à l'IUT  : afin d'identifier votre parcours et de vous envoyer les informations adéquates.</li>
+                                <li>L'année d'entrée à l'IUT</li>
+                            </ul>
+                        </div>
+
+                        <div class="row">
+                            <h5>
+                                <span id="informations_personnelles_personne_facultatives_titre">les informations facultatives sont :</span>
+                            </h5>
+                        </div> 
+
+                        <div class="row">
+                            <ul>
+                                <li>l'adresse postale de l'individu</li>
+                                <li>le numéro de téléphone</li>
+                                <li>Toute information relative à vos études ou profession actuelles et antérieures</li>
+                                <li>l'année de sortie de l'IUT</li>
+                                <li>La photo de profil</li>
+                            </ul>
+                        </div>
+
+                        <div class="row">
+                            En application de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, 
+                            chaque membre du réseau dispose des droits d'opposition, d'accès et de rectification des données le concernant. 
+                            Tout membre du réseau peut librement modifier ou effacer les informations qu’il a lui-même saisies sur le réseau. 
+                            Il peut par ailleurs exiger les rectifiées, complétées, clarifiées, mises à jour ou effacées les informations le 
+                            concernant via son profil ou en passant par la page de contact pour exiger la modifications des informations dont il n'a pas accés 
+                            par lui même.
+                            <br><br>
+                            Les informations contenues dans la base de données de l'Alumni de l'IUT de Lens restent la propriété des membres. 
+                            l'administration est seule autorisée à les détenir et s'engage à ne pas les revendre ou les transmettre à des tiers, 
+                            et à ce que ces informations ne soient pas traitées à d'autres fins que leur mise en ligne au sein du réseau. 
+                            Toutefois, dans le cadre de partenariats avec des associations d’anciens constituées en groupe au sein du réseau, 
+                            les données personnelles déposées par les membres des dites associations sur le réseau pourront leur être transmises avec 
+                            l’accord des membres concernés.
+                            <br><br>
+                            l'Alumni de l'IUT de Lens est protégé par les dispositions du Code de la propriété intellectuelle, 
+                            notamment sur la protection des bases de données. Toute personne qui se constituerait un fichier ou une base de données 
+                            avec les informations présentes sur le réseau, et qui le mettrait à disposition d'un tiers, à titre onéreux 
+                            ou gratuit, sans l'accord de l'Alumni et en violation de la présente charte, s'exposerait aux sanctions civiles et/ou 
+                            pénales prévues par le Code de la propriété intellectuelle.
+                            <br><br>
+                            Le membre du réseau reconnaît être informé des risques liés à l’usage d'Internet et des réseaux, et notamment du fait que des 
+                            informations relatives à des données personnelles le concernant peuvent être copiées ou interceptées. 
+                            Le membre du réseau reconnaît accepter ces risques, dont l'administration de ce site ne peut en aucun cas être tenue pour responsable. 
+                            Toutefois, cette même administration met de son côté en œuvre toutes les ressources techniques et humaines pour 
+                            assurer un niveau permanent de sécurité conforme aux règles de l’art en vigueur dans ce domaine et pour prévenir les 
+                            agressions de personnes malveillantes.
+                        </div>
+
+
+
                     </div>
-                    
                 </div>
+
+                <div class="row">
+                    <div id='forum_mp' class="col-md-offset-1">
+
+                        <div class="row">
+                            <h4>
+                                <span id="forum_mp_titre">Messages privés, Forums, circulation des messages </span>
+                            </h4>
+                            <hr>
+                        </div>
+
+                        <div class="row">
+
+                            Les Forums de discussion et les messages privés sont réservés aux membres du réseau et personnes dûment habilitées. 
+                            L'administration ne pourra être tenue pour responsable du contenu des messages distribués et transmis par 
+                            les membres au travers de ces moyens de discussions. Les membres du réseau acceptent que, s'ils existent, 
+                            les modérateurs des Forums exercent une surveillance sur les messages envoyés par les membres sur les Forums de discussion.
+
+                            Par ailleurs, les membres s'engagent à ce qu ele contenu de ses messages ne viole en aucune façon :
+                            <br><br>
+                            <ul>
+                                <li> les droits des personnes et des biens,</li>
+                                <li>les lois et réglementations en vigueur en France, dans son pays de résidence ou dans le pays où ses données peuvent être envoyées,</li>
+                                <li>et ne contienne ni virus, ni programmes susceptibles d’entraîner des dommages.</li>
+                            </ul>
+
+                            Il s'engage à ne pas transmettre de messages ou données illicites, diffamatoires, constituant des menaces ou attentatoires 
+                            aux bonnes mœurs. La transmission de tout message non sollicité à caractère publicitaire ou marchand ou émis dans un but politique, 
+                            syndical, religieux est interdite.
+                            <br><br>
+                            Le membre du réseau s'interdit l'envoi massif de courrier électronique aux autres membres du réseau. 
+                            Il s'engage à respecter l'image et la réputation de l'IUT de Lens et de son Alumni et à ne pas se livrer à des déclarations ou des actions pouvant 
+                            lui porter atteinte.
+                            <br><br>
+                            Le non respect de ces règles entraînera la radiation du réseau du membre auteur de message(s) litigieux et des poursuites de la part 
+                            du propriétaire du site. 
+
+
+
+
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div id='emplois_stages' class="col-md-offset-1">
+
+                        <div class="row">
+                            <h4>
+                                <span id="emplois_stages_titre">Offres d'emplois et de stages </span>
+                            </h4>
+                            <hr>
+                        </div>
+
+                        <div class="row">
+
+                            Tout membre du réseau ayant la possibilité de déposer ou de transmettre une offre d' emploi s’engage à ne pas utiliser de 
+                            critères interdits ou à caractère discriminatoire. En particulier sont interdites les offres dont l’accès est conditionné 
+                            aux critères suivants :
+                            <br><br>
+                            <ul>
+                                <li>l’origine</li>
+                                <li>le sexe,</li>
+                                <li>les mœurs,</li>
+                                <li>la grossesse,</li>
+                                <li>la situation familiale,</li>
+                                <li>la race, l’ethnie ou la nation,</li>
+                                <li>les opinions politiques,</li>
+                                <li>les activités syndicales ou mutualistes,</li>
+                                <li>les convictions religieuses,</li>
+                                <li>l’état de santé,</li>
+                                <li>le handicap,</li>
+                                <li>les orientations sexuelles,</li>
+                                <li>l’âge,</li>
+                                <li>l’apparence physique,</li>
+                                <li>le patronyme,</li>
+                                <li>les caractéristiques génétiques.</li>
+                            </ul>
+
+                            (Art. L 1132-1, L 1142-1, L 5531-2 du Code du travail)
+                        </div>
+
+
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div id='nonRespect' class="col-md-offset-1">
+
+                        <div class="row">
+                            <h4>
+                                <span id="nonRespect_titre">Non respect de la charte </span>
+                            </h4>
+                            <hr>
+                        </div>
+
+                        <div class="row">
+                            
+                            L'administration du site se réserve le doit de radier tout membre n’ayant pas respecté l’une des clauses de 
+                            la présente charte. L’accès du membre au réseau est en ce cas résilié de plein droit sans délai, 
+                            après envoi d’un message auprès de l'intéressé.
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div id='modification' class="col-md-offset-1">
+
+                        <div class="row">
+                            <h4>
+                                <span id=modification_titre">Modification de la charte</span>
+                            </h4>
+                            <hr>
+                        </div>
+
+                        <div class="row">
+                             La présente charte peut être modifiée à tout moment par l'asministration de l'Alumni, notamment pour être en conformité 
+                            avec des dispositions légales ou règlementaires nouvelles. Elle en informe les membres du réseau par publication 
+                            sur le site du réseau ou par diffusion d’un message aux membres au moyen d’une liste de diffusion.
+                            <br>
+                            Tout nouvel accès au réseau par un membre suite à la diffusion de la Charte modifiée vaut acceptation de celle-ci par le membre.
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div id='litiges' class="col-md-offset-1">
+
+                        <div class="row">
+                            <h4>
+                                <span id=litiges_titre">litiges</span>
+                            </h4>
+                            <hr>
+                        </div>
+
+                        <div class="row">
+                            La présente charte est régie par le Droit Français. Tout litige relatif à la validité, l'interprétation ou l'exécution de la 
+                            présente charte et après échec de toute conciliation, sera soumis aux tribunaux compétents.
+                        </div>
+                    </div>
                 </div>
                 
                 
+
+
+
             </div>
 
         </div>
