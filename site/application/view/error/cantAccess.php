64d302115bbcfab722192f54a8ca66759af0f887
Devil62 <devil.css@hotmail.fr>
2015-06-11 16:25:06 +0200

    Mise en page pages erreurs

--- site/application/view/error/cantAccess.php
+++ site/application/view/error/cantAccess.php
@@ -1,4 +1,4 @@
 <div style="text-align:center;">
-    Votre statut ne vous permet pas d'accéder à cette zone du site.
+    <div id="row"><img src="<?php echo URL ?>/img/stop.png" id="cant_access_img"></div>
     <div class="row"><button type="submit" class=" btn btn-default" id="retour_btn">Retour à l'accueil</button></div>
 </div>
--- site/application/view/error/index.php
+++ site/application/view/error/index.php
@@ -1,4 +1,5 @@
 <div style="text-align:center;">
-    <div class="row">Cette page n'existe pas... Êtes-vous perdu ?</div>
+    <div class="row">Cette page n'existe pas... Êtes-vous perdu ?....</div>
+    <div id="row"><img src="<?php echo URL ?>/img/vous_etes_ici.png" id="notfoundimg"></div>
     <div class="row"><button type="submit" class=" btn btn-default" id="retour_btn">Retour à l'accueil</button></div>
 </div>
